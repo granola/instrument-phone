07dbd85f2c3efb650e326214b7bb511d66d0d706
kaminora <kaminoura9207@gmail.com>
2019-09-15 14:36:35 +0900

    :sparkles: Add Express as api server

---
+++ backend/main.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+app.listen(3000, () => console.log('Listening on port 3000!'))
